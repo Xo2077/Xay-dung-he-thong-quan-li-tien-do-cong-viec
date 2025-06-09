8634f7ba3552f2b66c599cc3971a5497eb92a249
nguyen tan sang <cuonglaivan957@gmail.com>
2025-06-09 21:30:28 +0700

    Update phan-cong-cong-viec.cpp

--- phan-cong-cong-viec.cpp
+++ phan-cong-cong-viec.cpp
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<
+#include"quan-li-nhan-vien.cpp"
+#include"quan-li-cong-viec.cpp"
 void phan_cong_cong_viec() {
     if (so_luong_cong_viec >= MAX_TASKS || so_luong_nhan_vien == 0) {
         printf("Không thể phân công! Danh sách đầy hoặc chưa có nhân viên.\n");
